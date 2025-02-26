c5b46aa4ac0d29100d2d3fb3a1dd4d85783fa685
mo92othman <mo92othman@gmail.com>
2025-02-26 23:35:26 +0100

    addeda background to the screens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div> */}
+        <div className="bg-gray-400 absolute top-[-2rem] -z-10 left-[-30rem] h-[35rem] w-[55rem] rounded-full blur-[12rem] sm:w-[70rem] md:left-[-28rem] lg:left-[-20rem] xl:left-[-10rem] 2xl:left-[-2rem] dark:bg-gray-800"></div>
+
         <TanstackProvider>{children}</TanstackProvider>
       </body>
     </html>
