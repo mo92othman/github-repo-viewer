a7eb025f94192a8fe4a8b13729dbcb4e7842a6ed
mo92othman <mo92othman@gmail.com>
2025-02-26 11:11:37 +0100

    passed the user data to userInfo componenet and added types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-4xl font-bold text-center">Overview page</h1>
-      <p>{user.name}</p>
-      <UserInfo />
+
+      <UserInfo user={user} />
       <RepoList />
     </div>
   );
---
+++ src/lib/types.ts
@@ -0,0 +1,6 @@
+export type User = {
+  login: string;
+  avatar_url: string;
+  bio: string;
+  name: string;
+};
