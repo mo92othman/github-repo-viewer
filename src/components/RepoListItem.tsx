577d3242477494eb82109132c1e2d4f34884288b
mo92othman <mo92othman@gmail.com>
2025-02-27 13:23:15 +0100

    added truncate to the repo name in the repo list item component

--- src/components/RepoListItem.tsx
+++ src/components/RepoListItem.tsx
@@ -10,7 +10,7 @@ export default function RepoListItem({ name, description }: RepoListItemProps) {
     <li className="border p-4 rounded shadow transition transform hover:scale-105">
       <Link href={`/repo/${name}`}>
         <div>
-          <h2 className="text-xl font-semibold">{name}</h2>
+          <h2 className="text-xl font-semibold truncate">{name}</h2>
           <p className="text-gray-500">{description}</p>
         </div>
       </Link>
